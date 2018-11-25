c7b2179d04e35e6ccaa08a850d78b276a7576cc7
Jayce Azua <jayce.azua@gmail.com>
2018-11-25 03:35:32 -0800

    sql

--- dbConnection/mongo.js
+++ dbConnection/mongo.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/outbooked-demo';
 mongoose.connect(dbURI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
-  console.log('Connected.')
+  console.log('Mongo Connected.')
 });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
 mongoose.set('useCreateIndex', true);
--- dbConnection/sql.js
+++ dbConnection/sql.js
@@ -6,13 +6,19 @@ Download a free MySQL database at https://www.mysql.com/downloads/.
 const dbpassword = process.env.DB_PASSWORD;
 const dbuser = process.env.DB_USER;
 // Use the username and password from our MySQL database.
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
   host: "localhost",
   user: dbuser,
-  password: dbpassword
+  password: dbpassword,
+  database: "outbooked"
 });
 
-connection.connect((err) => {
+con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("DB Connected!");
+  var sql = "CREATE TABLE interviewers (access VARCHAR(255), token VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
 });
