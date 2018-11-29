288cef9945aebe03012daea1cdd9db0bb7c8acfe
Phyllis Wong <wong.phyllism@gmail.com>
2018-11-29 11:27:00 -0800

    Revert "connecting dbs"

--- helpers/auth.js
+++ helpers/auth.js
@@ -60,7 +60,8 @@ async function getAccessToken(cookies, res) {
     }
   }
 
-  // Either no token or it's expired, do we have a refresh token?
+  // Either no token or it's expired, do we have a
+  // refresh token?
   const refresh_token = cookies.graph_refresh_token;
   if (refresh_token) {
     const newToken = await oauth2.accessToken.create({refresh_token: refresh_token}).refresh();
@@ -72,7 +73,7 @@ async function getAccessToken(cookies, res) {
   return null;
 }
 
-function saveValuesToCookie(token, res) { // consider having the cookies expire every 6 months
+function saveValuesToCookie(token, res) {
   // Parse the identity token
   const user = jwt.decode(token.token.id_token);
 
@@ -93,10 +94,7 @@ function clearCookies(res) {
   res.clearCookie('graph_token_expires', {maxAge: 3600000, httpOnly: true});
 }
 
-
-module.exports = {
-    getAuthUrl,
-    getTokenFromCode,
-    getAccessToken,
-    clearCookies
-}
+exports.getAuthUrl = getAuthUrl;
+exports.getTokenFromCode = getTokenFromCode;
+exports.getAccessToken = getAccessToken;
+exports.clearCookies = clearCookies;
--- routes/authorize.js
+++ routes/authorize.js
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
-const express = require('express');
-const router = express.Router();
-const { getTokenFromCode, clearCookies } = require('../helpers/auth');
+var express = require('express');
+var router = express.Router();
+var authHelper = require('../helpers/auth');
 
 // Get authroize route
 // Handles returned authentication code from Microsoft's OAuth2 login
-router.get('/authorize', async function(req, res, next) {
+router.get('/', async function(req, res, next) {
   // Get auth code
   const code = req.query.code;
 
@@ -18,19 +18,16 @@ router.get('/authorize', async function(req, res, next) {
 
   // There is a code, so attempt to exchange it for a token
   try {
-    let token = await getTokenFromCode(code, res);
-    res.redirect('/'); // <- this might be your bug for it not allowing it to redirect.
+    let token = await authHelper.getTokenFromCode(code, res);
+    res.redirect('/');
   } catch (error) {
     res.json({ title: 'Error', message: 'Error exchanging code for token', error: error });
   }
 });
 
-
-
 // Signs the user out by clearing cookies
-router.get('/authorize/signout', function(req, res, next) {
-
-  clearCookies(res);
+router.get('/signout', function(req, res, next) {
+  authHelper.clearCookies(res);
 
   // Redirect to home
   res.redirect('/');
