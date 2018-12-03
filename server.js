76a2fa9f6f3a2d08a4331de29974936cc0755dcd
Jayce Azua <jayce.azua@gmail.com>
2018-12-03 13:21:36 -0800

    fixed typo

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
