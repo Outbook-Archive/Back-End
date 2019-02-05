a03066c6603316f3ad60ad2d66d2bf22895af100
Jayce Azua <jayce.azua@gmail.com>
2019-02-05 12:53:15 -0800

    Forgot to put `await`

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { getAuthUrl, getAccessToken } = require('../helpers/auth');
 
 // Redirect to OAuth2 login
 router.get('/', async function(req, res) {
-    const signInUrl = getAuthUrl();
+    const signInUrl = await getAuthUrl();
     res.redirect(signInUrl);
 })
 
