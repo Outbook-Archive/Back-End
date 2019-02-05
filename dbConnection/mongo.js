52090f15b2865bab8a2e4cbee6cbeeacd4d7d26e
Jayce Azua <jayce.azua@gmail.com>
2019-02-05 13:00:47 -0800

    Update mongo.js

--- dbConnection/mongo.js
+++ dbConnection/mongo.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
  // Set a Mongoose Promise Library
 mongoose.Promise = global.Promise;
-// const dbpassword = process.env.DB_PASSWORD; <- for mlab
-// const dbuser = process.env.DB_USER; <- for mlab
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/outbooked-demo';
 mongoose.connect(dbURI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
@@ -10,5 +8,5 @@ mongoose.connection.once('open', () => {
 });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
 mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false); // corrects the annoying DeprecationWarning
+mongoose.set('useFindAndModify', false); // corrects the Deprecation Warning
 // mongoose.set('debug', true);
