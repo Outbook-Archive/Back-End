bd97a3cb3dbe0a72d1e3b6067c59c3cf3e655438
Jayce Azua <jayce.azua@gmail.com>
2019-03-01 01:04:41 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cron = require('node-cron');
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
